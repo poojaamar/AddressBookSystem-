b598adb99930782d18b50fb191d389b992a72ea5
poojaamar <pamar6401@gmail.com>
2022-09-05 21:46:31 +0530

    Add Contact Method

--- AddressBookSystem/AddressBookSystem/Program.cs
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -2,6 +2,25 @@
 
 Console.WriteLine("Welcome to AddressBook Program.");
 AddressBook addressBook = new AddressBook(); 
-addressBook.addContact(); 
-addressBook.listContact(); 
-
+int option = 0;
+while (option != 3) 
+{
+    Console.WriteLine("Press 1 for add contact.\n Press 2 for list the contact.\n Press 3 to exit.");
+    Console.WriteLine("Please enter option number: ");
+    option = int.Parse(Console.ReadLine()); 
+    switch (option)
+    {
+        case 1: 
+            addressBook.addContact();
+            break;
+        case 2: 
+            addressBook.listContact();
+            break;
+        case 3:
+            Console.WriteLine("Exiting from Program.");
+            break;
+        default: 
+            Console.WriteLine("Wrong option.");
+            break;
+    }
+}
